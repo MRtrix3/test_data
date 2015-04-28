90953521937a5ea000876359cbe46382c786d47d
J-Donald Tournier <jdtournier@gmail.com>
2015-04-28 15:29:59 +0100

    add test for dwi2fod with mask supplied

--- tests/dwi2fod.sh
+++ tests/dwi2fod.sh
@@ -1,2 +1,3 @@
 dwi2fod dwi.mif response.txt - | testing_diff_data - dwi2fod/out.mif 1e-15
+dwi2fod dwi.mif -mask mask.mif response.txt - | testing_diff_data - dwi2fod/out_mask.mif 1e-15
 dwi2fod dwi.mif response.txt -lmax 12 - | testing_diff_data - dwi2fod/out_lmax12.mif 1e-15
